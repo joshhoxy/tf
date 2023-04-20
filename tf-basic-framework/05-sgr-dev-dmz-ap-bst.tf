20936c3c1443f8fa7bc3c3ec1ab85351e845e087
joshhoxy <whtmdghkszx@gmail.com>
2023-04-20 10:49:44 +0900

    sgdgfg

--- tf-basic-framework/05-sgr-dev-dmz-ap-bst.tf
+++ tf-basic-framework/05-sgr-dev-dmz-ap-bst.tf
@@ -7,3 +7,13 @@ resource "aws_security_group_rule" "sgr-dev-dmz-ap-bst-1" {
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "Allow SSH from Any"
 }
+
+resource "aws_security_group_rule" "sgr-dev-dmz-ap-bst-1" {
+  security_group_id = aws_security_group.sg-dev-dmz-ap-bst.id
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "TCP"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow HTTPs from Any"
+}
