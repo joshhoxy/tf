b1a4d468f25c685b50a8906b677b3696c8ac135b
jsohhoxy <whtmdghkszx@gmail.com>
2023-09-03 14:26:44 +0900

    modification variables.tf

--- basic/variables.tf
+++ basic/variables.tf
@@ -2,6 +2,10 @@ variable "vpc-dev-dmz-cidr" {
   default = "100.1.0.0/16"
 }
 
+variable "vpc-dev-int-cidr" {
+  default = "100.2.0.0/16"
+}
+
 variable "vpc-dev-dmz-name" {
   default = "SNBX-DEV-DMZ-VPC"
 }
