053fffd10e2af18ecbf48eaa8d63fd2009831ba2
jsohhoxy <whtmdghkszx@gmail.com>
2023-09-05 20:38:53 +0900

    add ec2

---
+++ basic/ec2/userdata-ec2-default.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+useradd -u 2001 sysadmin 
+usermod -G wheel sysadmin
+echo "ad12#$" | passwd --stdin sysadmin
+echo "root12#$" | passwd --stdin root
+sed -i 's/#PermitRootLogin/PermitRootLogin/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
